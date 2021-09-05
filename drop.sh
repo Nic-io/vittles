cf4eb43266f7d6dff84625fbfbc3b9acc1bece10
NBlaz <NBlaz@github.com>
2021-09-05 13:34:36 +0200

    i'm dumb

--- drop.sh
+++ drop.sh
@@ -8,16 +8,17 @@ case $1 in
         echo "vimrc already present. mv to .vimrc_old"
         mv ~/.vimrc ~/.vimrc_old
         cp .vimrc ~/.vimrc
-        mkdir -p ~/.vim/pack/plugins/start/
-        cd ~/.vim/pack/plugins/start/
-        echo "installing plugins"
-        git clone https://github.com/vim-airline/vim-airline.git
-        git clone https://github.com/preservim/nerdtree.git
-        git clone https://github.com/vim-scripts/AutoComplPop.git
-        mkdir -p ~/.vim/default/start/
-        cd  ~/.vim/default/start/
-        git clone  ~/.vim/default/start/
-      fi
+     fi
+     mkdir -p ~/.vim/pack/plugins/start/
+     cd ~/.vim/pack/plugins/start/
+     echo "installing plugins"
+     git clone https://github.com/vim-airline/vim-airline.git
+     git clone https://github.com/preservim/nerdtree.git
+     git clone https://github.com/vim-scripts/AutoComplPop.git
+     mkdir -p ~/.vim/pack/default/start/
+     cd  ~/.vim/pack/default/start/
+     git clone  https://github.com/morhetz/gruvbox.git
+    
       ;;
 
 esac
